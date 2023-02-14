ff7632981499f5ccc52b36bea6e6a388713b30aa
atsushi0610 <kogure.bbc@gmail.com>
2023-02-14 17:13:30 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    post = Post.create(content: params[:content]) #Postモデルで生成されたメモの内容を変数に代入
+    render json:{ post: post } #生成された変数postをpostキーとセットにして渡す
   end
 end
